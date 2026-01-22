f6cdd59dbf6cfb4063d84819739808773b9e8a0b
Kaleman60 <kalemancardenas@gmail.com>
2026-01-22 09:46:09 +0100

    se  ha hecho cambios en el index.ts

--- index.ts
+++ index.ts
@@ -1,10 +1,18 @@
 import { initDB } from "./db/init";
 import { getAllMovies, getMovieById, getMoviesByTitle } from "./models/movies";
 import express from "express";
+import type { Request, Response, NextFunction } from "express";
+
 const app = express();
 const PORT = 3000
 const db = await initDB()
 
+const logMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    console.log(req.method, req.url, new Date().toISOString())
+    next()
+}
+
+app.use(logMiddleware)
 
 app.get("/movies", (req, res) => {
     const peliculas = getAllMovies(db)
