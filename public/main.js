d1b9ecf2baf4daffdf14f32db67acefdcb9fbc1b
Kaleman60 <kalemancardenas@gmail.com>
2026-01-26 10:08:48 +0100

    se ha creado un archivo.css

--- public/main.js
+++ public/main.js
@@ -1,5 +1,10 @@
-fetch('url')
+/*fetch('url')
 .then(response => response.json())
 .then(data => {
     console.log(data);
-}) 
+})*/
+
+
+const respuesta = await fetch("/movies")
+const movies = await respuesta.json()
+console.log(movies)
